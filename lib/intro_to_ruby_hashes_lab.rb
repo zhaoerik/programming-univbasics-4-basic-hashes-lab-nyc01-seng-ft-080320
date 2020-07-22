5a4f01a4f65851829a004eaf8a611716cd56221e
Erik Zhao <erikzhao98@gmail.com>
2020-07-22 04:09:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -8,11 +8,11 @@ def my_hash
   {:breakfast => "cereal"}
 end
 
-# def pioneer
-#   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-# end
-#
-#
-# end
+def pioneer
+  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {:name => "Grace Hopper"}
+end
+
 # def id_hash_generator(number)
 #   # return a hash with a key :id assigned to the provided number
+# end
