17b9c211a0b613dd4d27bd5013ce79310ea2c033
Erik Zhao <erikzhao98@gmail.com>
2020-07-22 03:55:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,12 +6,12 @@ end
 # def my_hash
 #   # return a valid hash with any key/value pair of your choice
 # end
-# 
+#
 # def pioneer
 #   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 # end
-# 
-# 
+#
+#
 # def id_hash_generator(number)
 #   # return a hash with a key :id assigned to the provided number
 # end
