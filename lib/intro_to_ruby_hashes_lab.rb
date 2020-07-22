e276fc6932175d68a1940d4633c3f8eb4bef65a3
Erik Zhao <erikzhao98@gmail.com>
2020-07-22 04:09:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -13,6 +13,7 @@ def pioneer
   {:name => "Grace Hopper"}
 end
 
-# def id_hash_generator(number)
-#   # return a hash with a key :id assigned to the provided number
-# end
+def id_hash_generator(number)
+  # return a hash with a key :id assigned to the provided number
+  {:id => 1}
+end
